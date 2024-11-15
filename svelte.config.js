e3281547aa81e44943747f8b1ea51bb4c3fb3eb0
BrendanEichDisciple <BrendanEichDisciple@hotmail.com>
2024-11-15 14:53:49 +0800

    chore: 升级依赖包

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 
-import dotenv from 'dotenv';
-
-// 加载环境变量
-const envPath = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-dotenv.config({ path: envPath });
-// console.log(process.env); // 检查 VITE_BASE 是否正确加载
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -14,9 +7,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: process.env.VITE_BASE
-		}
 	}
 };
 
